6820eba48959a63aef4ea875999afdd1834f8a23
Andressa Fernandes <andressaf28@gmail.com>
2025-07-13 10:20:39 -0300

    ajustes no site

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 var one = document.getElementById("appear")
 		two = document.getElementById("disappear") 
-	one.onclick = function() { document.getElementById("box").style.display = "block";
-}
+  one.onclick = function() {
+	window.open('https://forms.gle/PEcH7xNMfJYcyEk96', '_blank');
+  }
 
 	two.onclick = function() { document.getElementById("box").style.display = "none";
 }
