af1d930c5f7c5baf0f31742bbb34f6af87e53adf
Andressa Fernandes <andressaf28@gmail.com>
2025-07-13 00:26:22 -0300

    site

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var one = document.getElementById("appear")
+		two = document.getElementById("disappear") 
+	one.onclick = function() { document.getElementById("box").style.display = "block";
+}
+
+	two.onclick = function() { document.getElementById("box").style.display = "none";
+}
